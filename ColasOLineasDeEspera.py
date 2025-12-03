cb31173742604842f28c421b6c8adaae5e1df55a
alejanold <iamaleortiz@gmail.com>
2025-12-03 16:07:13 -0600

    Se agrega el Proyecto

---
+++ ColasOLineasDeEspera.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------
+# Programa para calcular modelos de colas:
+# en este caso M/M/1, M/M/C y M/M/1/K
+# Investigacion de Operaciones
+# ----------------------------------------------
+# Variables a usar:
+# λ = tasa promedio de llegadas
+# μ = tasa promedio de servicio
+# C = numero de servidores
+# a = que tan cargado esta el sistema
+# ρ = utilizacion del sistema (que tanto se ocupa el servidor)
+# ----------------------------------------------
+import math
+
+def factorial(n):
+    return math.factorial(n)
