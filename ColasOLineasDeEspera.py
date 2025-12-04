f8d84cfd0bff07668962ecc39519a93dd931c459
José Alejandro Canales Ortiz <iamaleortiz@gmail.com>
2025-12-03 22:15:01 -0600

    Ahora con modelo MMC
    actualizacion del codigo pero ahora con el modelo MMC

--- ColasOLineasDeEspera.py
+++ ColasOLineasDeEspera.py
@@ -41,3 +41,42 @@ print("\n--- PROCEDIMIENTO ---")
 # ----------------------------------------------
 # MODELO M/M/C
 # ----------------------------------------------
+def modelo_mmc(lam, mu, c):
+    print("\n--- MODELO M/M/C ---")
+    print(f"λ = {lam}, μ = {mu}, C = {c}")
+
+    a = lam / mu  # intensidad de tráfico
+    rho = a / c   # utilización
+    print(f"\na = λ/μ = {a}")
+    print(f"ρ = a/C = {rho}")
+
+    if rho >= 1:
+        print("\n Sistema NO es estable porque ρ ≥ 1")
+        return
+
+    # Cálculo de P0 (para la probabilidad de 0 unidades en el sistema)
+    suma = 0
+    for n in range(c):
+        suma += (a**n) / factorial(n)
+
+    parte2 = (a**c) / (factorial(c) * (1 - rho))
+    P0 = 1 / (suma + parte2)
+
+    # Para la probabilidad de espera
+    Pw = ( (a**c) / (factorial(c) * (1 - rho)) ) * P0
+
+    # Para Lq
+    Lq = Pw * (rho / (1 - rho))
+
+    # L, W, Wq
+    L = Lq + a
+    W = L / lam
+    Wq = Lq / lam
+
+    print("\n--- PROCEDIMIENTO ---")
+    print(f"P0 = {round(P0, 6)}")
+    print(f"P(espera) = {round(Pw, 6)}")
+    print(f"Lq = {round(Lq, 4)}")
+    print(f"L = {round(L, 4)}")
+    print(f"Wq = {round(Wq, 4)}")
+    print(f"W = {round(W, 4)}")
